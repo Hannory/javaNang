a2e1d3dcc2fee17402209c561b7dd73a243e3f79
user <user@DESKTOP-723RNEO>
2019-11-17 15:38:32 +0900

    변경사항: 관리자모드 에러제거 레시피 업로드 메인없애기

--- src/view/manager/MainPage.java
+++ src/view/manager/MainPage.java
@@ -23,70 +23,68 @@ public class MainPage extends JPanel {
 
 	JFrame mf;
 	JPanel mp;
-	private JTextField textField;
-
-
-
 
 
 	public MainPage(JFrame mf) {
 
+		//필드값 초기화
 		this.mf = mf;
 		this.mp = this;
 
-
-		this.setBounds(0, 0, 432, 753);
+		//전체 패널 기본 설정 
+		this.setSize(445, 770);
 		this.setBackground(UIManager.getColor("CheckBox.background"));
 		this.setLayout(null);
-
-		/*JPanel panel_1 = new JPanel();
-		panel_1.setBounds(0, 0, 450, 800);
-		add(panel_1);
-		panel_1.setLayout(null);*/
-		/*contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(null);*/
-
+		
+		
 		mf.getContentPane().add(this);
 
+		//상단 패널 설정
+		JPanel toppanel = new JPanel();
+		toppanel.setBackground(new Color(63,141,197));
+		toppanel.setBounds(0,0,445,70);
+		mp.add(toppanel);
+		toppanel.setLayout(null);
+		
+				JPanel panel_3 = new JPanel();
+				panel_3.setBounds(0, 0, 432, 77);
+				toppanel.add(panel_3);
+				panel_3.setBackground(new Color(70, 130, 180));
+				panel_3.setLayout(null);
+				
+				JTextArea textArea = new JTextArea();
+				textArea.setBounds(117, 12, 197, 48);
+				panel_3.add(textArea);
+				textArea.setFont(new Font("문체부 쓰기 정체", Font.BOLD, 32));
+				textArea.setBackground(new Color(70, 130, 180));
+				textArea.setText("\uAD00\uB9AC\uC790 \uBAA8\uB4DC");
+		
+		//하단 패널 설정
+		JPanel bottpanel = new JPanel();
+		bottpanel.setBackground(Color.WHITE);
+		bottpanel.setBounds(0,70,445,700);
+		mp.add(bottpanel);
+		bottpanel.setLayout(null);
+	
+		
+		
+		
 		JButton btnNewButton = new JButton("\uB808\uC2DC\uD53C \uC2B9\uC778");
-		btnNewButton.setBounds(129, 265, 174, 58);
-		add(btnNewButton);
+		btnNewButton.setBounds(126, 157, 174, 58);
+		bottpanel.add(btnNewButton);
 		btnNewButton.setFont(new Font("문체부 쓰기 정체", Font.BOLD, 23));
 		//mp.add(btnNewButton);
 
 		JButton btnNewButton_1 = new JButton("\uD68C\uC6D0 \uC81C\uC7AC");
-		btnNewButton_1.setBounds(141, 476, 174, 58);
-		add(btnNewButton_1);
+		btnNewButton_1.setBounds(126, 374, 174, 58);
+		bottpanel.add(btnNewButton_1);
 		btnNewButton_1.setFont(new Font("문체부 쓰기 정체", Font.BOLD, 23));
 
-		JPanel panel_3 = new JPanel();
-		panel_3.setBounds(0, 0, 432, 77);
-		add(panel_3);
-		panel_3.setBackground(new Color(70, 130, 180));
-		panel_3.setLayout(null);
 
-		textField = new JTextField();
-		textField.setBackground(new Color(70, 130, 180));
-		textField.setFont(new Font("문체부 쓰기 정체", Font.BOLD, 32));
-		textField.setText("\uAD00\uB9AC\uC790 \uBAA8\uB4DC");
-		textField.setBounds(109, 12, 204, 65);
-		panel_3.add(textField);
-		textField.setColumns(10);
-		
-		JLabel label_1 = new JLabel("\uD68C\uC6D0 \uC81C\uC7AC");
-		label_1.setBounds(113, 546, 174, 50);
-		add(label_1);
-		
-		JLabel label = new JLabel("\uB808\uC2DC\uD53C \uC2B9\uC778");
-		label.setBounds(192, 220, 147, 18);
-		add(label);
-		
-		
-	
-		
-		btnNewButton_1.addMouseListener(new MyItem2());
+
 		btnNewButton.addMouseListener(new MyItem());
+		btnNewButton_1.addMouseListener(new MyItem2());
+	
 		mf.repaint();
 
 
@@ -105,7 +103,7 @@ public class MainPage extends JPanel {
 	class MyItem2 extends MouseAdapter{
 		@Override
 		public void mouseClicked(MouseEvent e) {
-			new ChangePanel().changePanel(mf, mp, new SecondPage__2(mf));
+			ChangePanel.changePanel(mf, mp, new SecondPage__2(mf));
 		}
 	}
 }
